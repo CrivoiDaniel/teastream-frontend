aeebfd815980a2c20b39c4514564ea063611dec4
Crivoi Daniel <crivoidan2004@gmail.com>
2026-01-04 18:20:50 +0200

    Added functionality for login

---
+++ src/components/features/auth/forms/LoginForm.tsx
@@ -0,0 +1,131 @@
+'use client'
+
+import { useForm } from "react-hook-form";
+import { AuthWrapper } from "../AuthWrapper";
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/common/Form";
+import { Input } from "@/components/ui/common/Input";
+import { Button } from "@/components/ui/common/Button";
+import { useLoginUserMutation } from "@/graphql/generated/output";
+import { toast } from "sonner";
+
+import { useTranslations } from "next-intl";
+import { loginSchema, TypeLoginSchema } from "@/schemas/auth/login.schema";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/components/ui/common/InputOTP";
+export function LoginForm() {
+
+    const t = useTranslations('auth.login')
+
+    const router = useRouter()
+
+    const [isShowTwoFactor, setIsShowTwoFactor] = useState(false)
+
+    const form = useForm<TypeLoginSchema>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            login: '',
+            password: ''
+        }
+    })
+
+    const [login, { loading: isLoadingLogin }] = useLoginUserMutation({
+        onCompleted(data) {
+            
+            if (data.loginUser.message) {
+                setIsShowTwoFactor(true)
+
+            } else {
+                toast.success(t('successMessage'))
+                router.push('/dashboard/settings')
+            }
+        },
+        onError() {
+            toast.error(t('errorMessage'))
+
+        }
+    })
+    const { isValid } = form.formState
+
+    function onSubmit(data: TypeLoginSchema) {
+        login({ variables: { data } })
+    }
+    return (
+        <AuthWrapper
+            heading={t('heading')}
+            backButtonLabel={t('backButtonLabel')}
+            backButtonHref='/account/create'
+        >
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-y-3">
+                    {isShowTwoFactor ? (
+                        <FormField
+                            control={form.control}
+                            name="pin"
+                            render={({ field }) =>
+                                <FormItem>
+                                    <FormLabel>{t('pinLabel')}</FormLabel>
+                                    <FormControl>
+                                        <InputOTP maxLength={6} {...field}>
+                                            <InputOTPGroup>
+                                                <InputOTPSlot index={0}></InputOTPSlot>
+                                                <InputOTPSlot index={1}></InputOTPSlot>
+                                                <InputOTPSlot index={2}></InputOTPSlot>
+                                                <InputOTPSlot index={3}></InputOTPSlot>
+                                                <InputOTPSlot index={4}></InputOTPSlot>
+                                                <InputOTPSlot index={5}></InputOTPSlot>
+                                            </InputOTPGroup>
+                                        </InputOTP>
+                                    </FormControl>
+                                    <FormDescription>{t('pinDescription')}</FormDescription>
+                                </FormItem>
+                            }
+                        />
+                    ) : (
+                        <>
+                            <FormField
+                                control={form.control}
+                                name="login"
+                                render={({ field }) =>
+                                    <FormItem>
+                                        <FormLabel>{t('loginLabel')}</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="johndoe"
+                                                disabled={isLoadingLogin}
+                                                {...field} />
+                                        </FormControl>
+                                        <FormDescription>{t('loginDescription')}</FormDescription>
+                                    </FormItem>
+                                }
+                            />
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) =>
+                                    <FormItem>
+                                        <FormLabel>{t('passwordLabel')}</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="********"
+                                                type="password"
+                                                disabled={isLoadingLogin}
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormDescription>{t('passwordDescription')}</FormDescription>
+                                    </FormItem>
+                                }
+                            />
+                        </>
+                    )}
+                    <Button className="mt-2 w-full" disabled={!isValid || isLoadingLogin}>
+                        {t('submitButton')}
+                    </Button>
+                </form>
+            </Form>
+        </AuthWrapper>
+    )
+}
